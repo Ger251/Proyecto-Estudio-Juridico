889b9306cb6bf65eb5a6e484c7fd5b59848cc109
German Jara <germanjara8@gmail.com>
2023-01-20 16:00:39 -0300

    Primera Maqueta (cambiar fotos)

---
+++ Js/Carrusel.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+//document.getElementById('Izquierda1').onclick = function mover (){
+
+ //   console.log("capuramos el click");
+   // document.getElementById('Mov').style.transform = 'translateX(-66%)';
+
+//}
+document.getElementById('Izquierda2').onclick = function mover (){
+
+    console.log("capuramos el click");
+    document.getElementById('Mov').style.transform = 'translateX(0%)';
+
+}
+document.getElementById('Izquierda3').onclick = function mover (){
+
+    console.log("capuramos el click");
+    document.getElementById('Mov').style.transform = 'translateX(-33%)';
+
+}
+
+document.getElementById('Derecha1').onclick = function mover (){
+
+    console.log("capuramos el click");
+    document.getElementById('Mov').style.transform = 'translateX(-33%)';
+
+}
+
+document.getElementById('Derecha2').onclick = function mover (){
+
+    console.log("capuramos el click");
+    document.getElementById('Mov').style.transform = 'translateX(-66%)';
+
+}
+//document.getElementById('Derecha3').onclick = function mover (){
+
+  //  console.log("capuramos el click");
+    //document.getElementById('Mov').style.transform = 'translateX(0%)';
+
+//}
